ba47ad51d1fbe85e08354d604253534d4c7411c1
scxfjiang <xuefei.jiang@amd.com>
2024-08-15 20:32:05 +0000

    update

--- tf_exercise/xla_kernel_fusion.py
+++ tf_exercise/xla_kernel_fusion.py
@@ -1,4 +1,11 @@
+import os
+os.environ['TF_XLA_FLAGS'] = '--tf_xla_auto_jit=2'
+os.environ['XLA_FLAGS'] = '--xla_dump_to=./generated'
+os.environ['TF_DUMP_GRAPH_PREFIX'] = './generated'
+os.environ['TF_XLA_FLAGS'] = '--tf_xla_clustering_debug'
+
 import tensorflow as tf
+tf.debugging.set_log_device_placement(True)
 
 @tf.function(jit_compile=True)
 def fused_conv_bias_relu(x):
@@ -8,5 +15,7 @@ def fused_conv_bias_relu(x):
     return y
 
 x = tf.random.normal((10, 3, 128, 128))
+tf.profiler.experimental.start('logdir')
 y = fused_conv_bias_relu(x)
+tf.profiler.experimental.stop()
 print(y.shape)
