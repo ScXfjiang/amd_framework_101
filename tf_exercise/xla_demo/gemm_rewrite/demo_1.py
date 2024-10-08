8a449ba29c49c58338ea215fc347b05cc659ea24
scxfjiang <xuefei.jiang@amd.com>
2024-10-08 08:54:04 -0500

    update

---
+++ tf_exercise/xla_demo/gemm_rewrite/demo_1.py
@@ -0,0 +1,21 @@
+import os
+
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
+# os.environ['TF_CPP_MAX_VLOG_LEVEL'] = '2'
+os.environ["XLA_FLAGS"] = (
+    "--xla_dump_to=./generated --xla_dump_hlo_as_dot --xla_dump_hlo_as_text --xla_dump_hlo_as_html --xla_dump_hlo_pass_re=.*"
+)
+
+import tensorflow as tf
+
+# (kMultiply (kCustomCall:gemm A B) C) is folding C (provided it's a constant)
+# into an alpha parameter of the custom call.
+@tf.function(jit_compile=True)
+def matmul_demo():
+    m1 = tf.constant([[1.0, 2.0], [3.0, 4.0]], tf.float32)
+    m2 = tf.constant([[5.0, 6.0], [7.0, 8.0]], tf.float32)
+    c = tf.constant(1.0, tf.float32)
+    return c * tf.matmul(m1, m2)
+
+res = matmul_demo()
+print(res)
