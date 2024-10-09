e8d92265a45e6ddc67bf7be7a22d06a373ea4661
scxfjiang <xuefei.jiang@amd.com>
2024-10-09 08:00:30 -0500

    update

--- tf_exercise/xla_demo/gemm_rewrite/fp8_matmul.py
+++ tf_exercise/xla_demo/gemm_rewrite/fp8_matmul.py
@@ -15,21 +15,29 @@ import tensorflow as tf
 
 @tf.function(jit_compile=True)
 def fp8_matmul(x_fp8, y_fp8, scale_x, scale_y, scale_z):
-    # 1. dequantize x_fp8 and y_fp8
+    # 1. dequantize x_fp8 and y_fp8 to fp16
     x_fp16_unscaled = tf.cast(x_fp8, tf.float16) * scale_x
     y_fp16_unscaled = tf.cast(y_fp8, tf.float16) * scale_y
     # 2. perform matmul in fp16
     z_fp16_unscaled = tf.matmul(x_fp16_unscaled, y_fp16_unscaled)
-    # 3. quantize z_fp16 and calculate new scale
+    # 3. quantize z_fp16 to fp8
     z_fp8 = tf.cast(z_fp16_unscaled / scale_z, tf.dtypes.experimental.float8_e4m3fnuz)
-    max_e4m3fnuz = 240
-    new_z_scale = max_e4m3fnuz / tf.reduce_max(tf.abs(z_fp16_unscaled))
-    return z_fp8, new_z_scale
+    return z_fp8
 
+@tf.function(jit_compile=True)
+def fp16_matmul(x_fp16, y_fp16):
+    return tf.matmul(x_fp16, y_fp16)
 
 if __name__ == "__main__":
-    x_fp8 = tf.constant([[1.0, 2.0], [3.0, 4.0]], tf.dtypes.experimental.float8_e4m3fnuz)
-    y_fp8 = tf.constant([[5.0, 6.0], [7.0, 8.0]], tf.dtypes.experimental.float8_e4m3fnuz)
-    z_fp8, new_z_scale = fp8_matmul(x_fp8, y_fp8, 2.0, 2.0, 2.0)
+    x_fp16 = tf.constant([[2.0, 2.0], [2.0, 2.0]], tf.float16)
+    y_fp16 = tf.constant([[4.0, 4.0], [4.0, 4.0]], tf.float16)
+    z_fp16 = fp16_matmul(x_fp16, y_fp16)
+    print(z_fp16)
+
+    x_fp8 = tf.cast(x_fp16, tf.dtypes.experimental.float8_e4m3fnuz)
+    y_fp8 = tf.cast(y_fp16, tf.dtypes.experimental.float8_e4m3fnuz) 
+    scale_x = tf.constant(2.0, dtype=tf.float16)
+    scale_y = tf.constant(2.0, dtype=tf.float16)
+    scale_z = tf.constant(4.0, dtype=tf.float16)
+    z_fp8 = fp8_matmul(x_fp8, y_fp8, scale_x, scale_y, scale_z)
     print(z_fp8)
-    print(new_z_scale)
