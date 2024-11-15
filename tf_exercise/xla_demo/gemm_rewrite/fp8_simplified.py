b42df584693b3d990e3ad2820ed4a1c49d4f8c41
scxfjiang <xuefei.jiang@amd.com>
2024-11-15 08:16:31 -0600

    update

--- tf_exercise/xla_demo/gemm_rewrite/fp8_simplified.py
+++ tf_exercise/xla_demo/gemm_rewrite/fp8_simplified.py
@@ -9,6 +9,7 @@ os.environ["XLA_FLAGS"] = (
     "--xla_dump_hlo_as_text "
     "--xla_dump_hlo_as_html "
     "--xla_dump_hlo_pass_re=.*"
+    # "--xla_gpu_enable_triton_gemm=false"
     # "--xla_gpu_gemm_rewrite_size_threshold=0"
 )
 
@@ -17,22 +18,22 @@ import numpy as np
 
 @tf.function(jit_compile=True)
 def fp8_matmul(x_fp8, y_fp8, scale_x, scale_y):
-    # 1. dequantize x_fp8 and y_fp8 to fp16
-    x_fp16_unscaled = tf.cast(x_fp8, tf.float16) * scale_x
-    y_fp16_unscaled = tf.cast(y_fp8, tf.float16) * scale_y
-    # 2. perform matmul in fp16
-    z_fp16_unscaled = tf.matmul(x_fp16_unscaled, y_fp16_unscaled)
-    return z_fp16_unscaled
+    # 1. dequantize x_fp8 and y_fp8 to fp32
+    x_fp32_unscaled = tf.cast(x_fp8, tf.float32) * scale_x
+    y_fp32_unscaled = tf.cast(y_fp8, tf.float32) * scale_y
+    # 2. perform matmul in fp32
+    z_fp32_unscaled = tf.matmul(x_fp32_unscaled, y_fp32_unscaled)
+    return z_fp32_unscaled
 
 if __name__ == "__main__":
-    x_fp16 = tf.random.uniform([16, 32], dtype=tf.float16)
-    y_fp16 = tf.random.uniform([32, 16], dtype=tf.float16)
+    x_fp32 = tf.random.uniform([16, 32], dtype=tf.float32)
+    y_fp32 = tf.random.uniform([32, 16], dtype=tf.float32)
     # initialize x_fp8 and y_fp8
-    x_fp8 = tf.cast(x_fp16, tf.dtypes.experimental.float8_e4m3fnuz)
-    y_fp8 = tf.cast(y_fp16, tf.dtypes.experimental.float8_e4m3fnuz)
+    x_fp8 = tf.cast(x_fp32, tf.dtypes.experimental.float8_e4m3fnuz)
+    y_fp8 = tf.cast(y_fp32, tf.dtypes.experimental.float8_e4m3fnuz)
     # initialize scale_x and scale_y
-    scale_x = tf.constant(2.0, dtype=tf.float16)
-    scale_y = tf.constant(2.0, dtype=tf.float16)
+    scale_x = tf.constant(2.0, dtype=tf.float32)
+    scale_y = tf.constant(2.0, dtype=tf.float32)
     # do fp8 matmul
-    z_fp8 = fp8_matmul(x_fp8, y_fp8, scale_x, scale_y)
-    print(z_fp8)
+    z_fp32 = fp8_matmul(x_fp8, y_fp8, scale_x, scale_y)
+    print(z_fp32)
